2cb7173d2c44509489867643518d0b06c02330a5
Gursewak Singh <gursmangat@gmail.com>
2020-09-07 11:49:55 -0700

    Update AboutMe

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class AboutMe extends Component {
     render() {
         return(
-            <div><h1> AboutMe </h1></div> 
+            <div><h1> I am a fourth year Computer Science student at the University of Victoria. </h1></div> 
         )
     }
 }
